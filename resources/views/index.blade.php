551d0d0abbe069c050655ae7d96465d417550f00
ALI <aliourihi@hotmail.fr>
2024-12-30 23:37:12 +0100

    view setup: tasks : display, completed?, delete, add

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,6 +10,28 @@
 	@if (session('success'))
 		<p>{{ session('success') }}</p>
 	@endif
-	
+	<ul>
+		@foreach ($tasks as $task)
+			<li>
+				{{ $task->title }} - {{ $task->description }}
+				@if ($task->completed)
+					<span style="color: green;">Completed</span>
+				@endif
+				<form action="/tasks/{{ $task->id }}" method="POST">
+					@csrf
+					@method('DELETE')
+					<button type="submit">Delete</button>
+				</form>
+			</li>
+		@endforeach
+	</ul>
+	<form action="/tasks" method="POST">
+		@csrf
+		<label for="title">Title</label>
+		<input type="text" name="title" id="title">
+		<label for="description">Description</label>
+		<input type="text" name="description" id="description">
+		<button type="submit">Add Task</button>
+	</form>
 </body>
 </html>
