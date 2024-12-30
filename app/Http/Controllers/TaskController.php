f2a8cffb03952566c7fbccea282c3506df2bc586
ALI <aliourihi@hotmail.fr>
2024-12-30 23:16:26 +0100

    controller setup 2: show, add

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -6,5 +6,25 @@ use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+    public function index()
+	{
+		$tasks = Task::where('user_id', Auth::id())->get();
+		return view('tasks.index', compact('tasks'));
+	}
+
+	public function store(Request $request)
+	{
+		$request->validate([
+			'title' => 'required|unique:tasks|max:255',
+			'description' => 'nullable|string',
+		]);
+
+		$task = new Task();
+		$task->user_id = Auth::id();
+		$task->title = $request->title;
+		$task->description = $request->description;
+		$task->save();
+
+		return redirect()->back()->with('success', 'Task created successfully.');
+	}
 }
