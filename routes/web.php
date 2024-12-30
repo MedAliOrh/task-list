94f12b3d5bdcdb633d474084708399f7dce1bf49
ALI <aliourihi@hotmail.fr>
2024-12-30 23:40:39 +0100

    routing

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/app', function () {
-    return view('app');
+
+Route::middleware(['auth'])->group(function () {
+	Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
+	Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
+	Route::put('/tasks/{id}', [TaskController::class, 'update'])->name('tasks.update');
+	Route::delete('/tasks/{id}', [TaskController::class, 'destroy'])->name('tasks.destroy');
 });
 
